8667caafe6241c6a8c02aeb20a2535bbd7e707cb
Spencer Fowler <spencer.fowler@is4s.com>
2019-04-20 18:04:34 -0500

    Add auto DSDL message building for ESP-IDF

--- component.mk
+++ component.mk
@@ -1,5 +1,17 @@
-COMPONENT_ADD_INCLUDEDIRS := . ./drivers/esp ./dsdl_compiler/dsdlc_generated
+
+DSDLC_PATH := $(COMPONENT_PATH)/dsdl_compiler/libcanard_dsdlc
+DSDL_PATH := $(COMPONENT_PATH)/dsdl_compiler/pyuavcan/uavcan/dsdl_files/uavcan
+DSDL_HEADERS_PATH := $(COMPONENT_PATH)/dsdl_compiler/dsdlc_generated
+
+COMPONENT_ADD_INCLUDEDIRS := . ./drivers/esp $(DSDL_HEADERS_PATH)
 COMPONENT_SRCDIRS := . ./drivers/esp
 CXXFLAGS += -fno-rtti
 
 CFLAGS += -D LOG_LOCAL_LEVEL=ESP_LOG_VERBOSE
+
+COMPONENT_EXTRA_CLEAN := $(shell find $(DSDL_HEADERS_PATH) -type f)
+
+canard.o: dsdl_headers
+
+dsdl_headers:
+	$(PYTHON) $(DSDLC_PATH) -v --header_only --outdir $(DSDL_HEADERS_PATH) $(DSDL_PATH)
